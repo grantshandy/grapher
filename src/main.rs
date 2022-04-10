c862f9450137bbd6050f58d2158e3ca87589769e
grantshandy <grantshandy@gmail.com>
2022-04-10 00:08:12 -0700

    add better windows support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 fn main() {
     eframe::run_native(
         Box::new(grapher::Grapher::new()),
