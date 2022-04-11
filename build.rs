a4c53c2de191d8d5ff263e1feec79e9b2561158a
grantshandy <grantshandy@gmail.com>
2022-04-11 11:24:31 -0600

    fix build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
+#[cfg(target_os = "windows")]
 extern crate winres;
 
 fn main() {
-    if cfg!(target_os = "windows") {
-      let mut res = winres::WindowsResource::new();
-      res.set_icon("public\\icon.ico");
-      res.compile().unwrap();
-    }
+  #[cfg(target_os = "windows")]
+  {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("public\\icon.ico");
+    res.compile().unwrap();
+  }
 }
